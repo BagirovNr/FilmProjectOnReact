7272e029ec180f12e4d980b3f02a86e5ec618fb7
Nadir <nadir.bagirov2000@gmail.com>
2025-08-06 18:08:44 +0400

    first commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
 import './global.css';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
+      <View className='justify-center mt-20'>
+        <Text className='text-primary '>hello wodfrld</Text>
+      </View>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
-
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#030014',
+        secondary: '#151312',
+        light: {
+          100: '#d6c6ff',
+          200: '#a8b5db',
+          300: '#9ca4ab',
+        },
+        dark: {
+          100: '#221f3d',
+          200: '#0f0d23',
+        },
+        accent: '#AB8BFF',
+      },
+    },
   },
   plugins: [],
 };
